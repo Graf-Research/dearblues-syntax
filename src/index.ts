20ff33c5654900e425396d9d6a9f716f990b1c1c
Rio on Mac <riochr17@gmail.com>
2022-05-27 13:15:23 +0700

    checkpoint

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+import nearley from "nearley";
+import { ObjectGrammar } from "./db-syntax.interface";
+import { performStaticChecks } from "./static-analytic";
+
+export class DearbluesParser {
+  private grammar = require("../grammar/compiled/db-syntax");
+  private parser = new nearley.Parser(nearley.Grammar.fromCompiled(this.grammar));
+
+  constructor() {
+    //
+  }
+
+  public parse(code: string): ObjectGrammar[] {
+    try {
+      this.parser.feed(code.trim());
+
+      const res: ObjectGrammar[] = this.parser.results[0];
+      performStaticChecks(res);
+      const total = {
+        table: res.reduce((acc, curr) => acc + (curr.type === 'table' ? 1 : 0), 0),
+        enum: res.reduce((acc, curr) => acc + (curr.type === 'enum' ? 1 : 0), 0),
+      };
+      console.log(`Stats: table = ${total.table}, enum = ${total.enum}`);
+      console.log(`Ambiguity: ${this.parser.results.length}`);
+      return res;
+    } catch (e: any) {
+      throw new Error(e.message);
+    }
+  }
+}
---
+++ src/test.ts
@@ -0,0 +1,10 @@
+import fs from 'fs';
+import { DearbluesParser } from ".";
+
+try {
+  const parser = new DearbluesParser();
+  const source_code = fs.readFileSync('./sample.dbs', 'utf8');
+  parser.parse(source_code);
+} catch (e: any) {
+  console.log(e.message);
+}
