a1d0a285bb4083249d613b58e423db5c87533145
Rio on Mac <riochr17@gmail.com>
2022-05-29 09:35:02 +0700

    checkpoint

---
+++ specs.d.ts
@@ -0,0 +1,25 @@
+import { DearbluesParser } from "./src";
+import { 
+  MooValue,
+  ObjectGrammar,
+  ObjectGrammarEnum,
+  ObjectGrammarTable,
+  ObjectGrammarTableItem,
+  ObjectGrammarTableItemEnum,
+  ObjectGrammarTableItemPrimitive,
+  ObjectGrammarTableItemReference,
+  ObjectGrammarTableOptionsDefaultValue 
+} from "./src/db-syntax.interface";
+
+export {
+  DearbluesParser,
+  MooValue,
+  ObjectGrammar,
+  ObjectGrammarEnum,
+  ObjectGrammarTable,
+  ObjectGrammarTableItem,
+  ObjectGrammarTableItemEnum,
+  ObjectGrammarTableItemPrimitive,
+  ObjectGrammarTableItemReference,
+  ObjectGrammarTableOptionsDefaultValue 
+}
